abb467ec8fc2bf4aa74322265c6e7b709280f4ae
MrSquirrel228 <League@imac-3.attlocal.net>
2018-06-14 20:01:12 -0700

    UnHappybirthday

---
+++ src/Unbirthday.java
@@ -0,0 +1,13 @@
+import javax.swing.JOptionPane;
+
+public class Unbirthday {
+public static void main(String[] args) {
+	String day;
+	day = JOptionPane.showInputDialog("What is your birthday (ex. 12/22)");
+	if (day.equals("6/14")) {
+		JOptionPane.showMessageDialog(null, "Happy Birthday");
+	}
+		
+	}
+}
+
